c2a7b620b855590afac90b3209b9bf353e8a5cef
Luka Matovic <lukamatovic1994@gmail.com>
2024-01-03 02:26:28 -0600

    Make API calls  on the server

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
 import express from "express";
 import bodyParser from "body-parser";
+import axios from "axios";
+import "dotenv/config";
 
 const app = express();
+const port = 3000;
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.render ("index.ejs")
-})
+  res.render("index.ejs");
+});
 
+app.post("/add", async (req, res) => {
+  const apiKey = process.env.API_KEY;
+  const { city } = req.body;
+  console.log(city);
+  try {
+    const coordinatesResponse = await axios.get(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`
+    );
+    const data = coordinatesResponse.data;
+    const lat = data[0].lat;
+    const lon = data[0].lon;
+    const cityResponse = await axios.get(
+      `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,daily&appid=${apiKey}&units=metric`
+    );
+    res.status(200).render("index.ejs", { data: cityResponse.data });
+  } catch (error) {
+    const err = "Location you entered does not exist. Please try again! ";
+    console.log(err);
+    res.render("index.ejs", {
+      error: "Location you entered does not exist. Please try again!",
+    });
+  }
 
-const port = 3000;
+  res.render("index.ejs");
+});
+
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
